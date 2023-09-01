c48b248abb4cb6135b67c031db7c2c0a0c091ef3
ghazanfarshewa <ghazanfarshewa29@gmail.com>
2023-09-01 11:05:24 +0500

    updated

--- members/urls.py
+++ members/urls.py
@@ -8,7 +8,6 @@ from . import views
 
 urlpatterns = [
     path('', views.members, name='members'),
-    path('members/', views.members, name='members'),
     path('details/<int:id>', views.details, name='details'),
      
 ] 
